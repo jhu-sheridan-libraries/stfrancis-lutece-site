616873bc3497608ec5c830325aafff00340b9542
jrmartino <james.r.martino@gmail.com>
2021-02-13 12:39:04 -0500

    configuration changes for internationalization remove local commons_bs4_materialkit.html file
    # Conflicts:
    #	pom.xml
    #	webapp/WEB-INF/templates/commons_bs4_materialkit.html

---
+++ src/sql/init_db_lutece_core_stfrancis_local.sql
@@ -0,0 +1,10 @@
+--
+-- Local updates to set locale for admin users, default locale for users, and portal locale
+--
+
+UPDATE core_admin_user SET locale = 'en' WHERE id_user = 1;
+UPDATE core_admin_user SET locale = 'en' WHERE id_user = 2;
+UPDATE core_admin_user SET locale = 'en' WHERE id_user = 3;
+UPDATE core_admin_user SET locale = 'en' WHERE id_user = 4;
+UPDATE core_datastore SET entity_value = 'en' WHERE entity_key = 'core.advanced_parameters.default_user_language';
+UPDATE core_datastore SET entity_value = 'en' WHERE entity_key = 'portal.site.site_property.locale.default';
